f3036d1392f1e5fdac74bd932b55e968d21af29d
Justin Fife <justin.garrett.fife@gmail.com>
2017-12-09 18:36:49 -0700

    Done

--- Mandelbrot/Program.fs
+++ Mandelbrot/Program.fs
@@ -9,11 +9,44 @@ let xsize = 800
 let ysize = 600
 let iters = 1000
 
+let mandel (maxiters : int, x : float, y : float) =
+    let mutable a = x
+    let mutable b = y
+    let mutable result = 0
+    for i in 1 .. maxiters do
+        let a2 = a * a
+        let b2 = b * b
+        if a2 + b2 >= 4.0 then result <- i
+        let ab = a * b
+        b <- ab + ab + y
+        a <- a2 - b2 + x
+    result
+
+let calcPixel(col : int, row : int, sizeX : int, sizeY : int, centerX : float, centerY : float, magnification : float) =
+    let mutable minsize = sizeX
+    if sizeY < sizeX then
+        minsize <- sizeY
+    let x = centerX + float (col - sizeX/2) / (magnification * float (minsize-1))
+    let y = centerY - float (row - sizeY/2) / (magnification * float (minsize-1))
+    let i = mandel (iters, x, y)
+    if i = 0 then
+        (0, 0, 0)
+    else
+        (0, i%256, 0)
+
+
 let generateImage =
-    let s = sprintf "P3\n%d %d\n255\n" xsize ysize
+    let s = sprintf "P3\n%d %d\n255\n" xsize ysize 
     File.WriteAllText(filePath, s)
-    File.AppendAllText(filePath, "255 245 255 ")
-    File.AppendAllText(filePath, "235 245 225")
+    for row in 0 .. ysize-1 do
+        let mutable strings = []
+        for col in 0 .. xsize-1 do
+            let r, g, b = calcPixel(col, row, xsize, ysize, xcenter, ycenter, mag)
+            let rgb = sprintf "%d %d %d" r g b
+            strings <- List.append strings [rgb]
+        strings <- List.append strings ["\n"]
+        File.AppendAllText(filePath, String.concat " " strings)
+        printfn "%i" row
 
 [<EntryPoint>]
 let main argv = 
